2c08cd08851781bd3c61189d29ba5b446197cfca
scridonlucas <lukas_scridon@yahoo.com>
2023-01-06 11:36:29 +0200

    Final version

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Tic-Tac-Toe
 
 Tic tac toe game created using HTML, CSS and Javascript. The purpose of the project was to practice the Module Pattern and the Factory Pattern. The game is created for two players.
+
+- [Live Preview](https://scridonlucas.github.io/Tic-Tac-Toe)
--- script.js
+++ script.js
@@ -4,16 +4,18 @@ const createGrid = (() => {
   for (let i = 0; i < 9; i++) {
     let gridElement = document.createElement('div');
     gridElement.classList.add('grid-element');
-    gridElement.setAttribute('id', i); // Assing each grid container's id according to array position.
+    gridElement.setAttribute('id', i); // Set each grid container's id according to array position.
     container.insertAdjacentElement('beforeend', gridElement);
   }
 })();
 
 const gameBoard = (() => {
   let gameBoardArray = ['', '', '', '', '', '', '', '', ''];
+
   function addToArray(position, symbol) {
     gameBoardArray[position] = symbol;
   }
+
   function returnArray() {
     return gameBoardArray;
   }
@@ -58,37 +60,41 @@ const playGame = (() => {
   let round = 0;
   let gameOver = false;
 
-  function addMark() {
-    grids.forEach((markedGrid) =>
-      markedGrid.addEventListener('click', markGrid)
-    );
-  }
+  grids.forEach((markedGrid) => markedGrid.addEventListener('click', markGrid));
 
   restartBtn.addEventListener('click', resetGrid);
 
   function resetGrid(e) {
     gameBoard.clearArray();
     gameBoard.render();
+    gameOver = false;
+    round = 0;
     currentPlayer = Player1;
+    turnText.textContent = `${Player1.name}, make your turn!`;
   }
 
   function markGrid(e) {
     const idIndex = e.target.id; // sau this.id, extract grid element's ID
-
-    if (gameBoardArray[idIndex] === '') {
-      //if it's empty, you can mark it
+    if (gameBoardArray[idIndex] === '' && gameOver === false) {
+      //if it's empty, you can mark it, game must not be over
       if (currentPlayer === Player1) {
         e.target.style.color = '#22d3ee';
         gameBoard.addToArray(idIndex, 'X');
-        currentPlayer = Player2;
-        turnText.textContent = `${Player2.name}, make your turn!`;
+        round++; //Check for winner after every marked box
+        checkWinner();
+        if (gameOver === false) {
+          // Change player only if game is not over
+          changePlayer();
+        }
       } else {
         gameBoard.addToArray(idIndex, 'O');
         e.target.style.color = '#dc2626';
-        currentPlayer = Player1;
-        turnText.textContent = `${Player1.name}, make your turn!`;
+        round++;
+        checkWinner();
+        if (gameOver === false) {
+          changePlayer();
+        }
       }
-
       gameBoard.render();
     }
   }
@@ -106,13 +112,29 @@ const playGame = (() => {
       [2, 4, 6],
     ];
 
-    winCombos.forEach((combo) => {
-      if (combo[0] === combo[1] && combo[1] === combo[2]) {
+    for (let i = 0; i < winCombos.length; i++) {
+      //Go through each combo
+      const combo = winCombos[i];
+      const a = gameBoardArray[combo[0]];
+      const b = gameBoardArray[combo[1]];
+      const c = gameBoardArray[combo[2]];
+      //Check if there are 3 matching syombol of all possible winning combinations (based on positions)
+      if (a === '' || b === '' || c === '') {
+        continue;
+      } else if (a === b && b === c) {
+        turnText.textContent = `${currentPlayer.name}, won!`;
+        gameOver = true;
+        break;
+      } else if (round === 9) {
+        //If 9 rouds pass and there is still no winner, then it's a tie
+        turnText.textContent = "It's a tie!";
+        gameOver = true;
       }
-    });
+    }
   }
 
-  addMark();
+  function changePlayer() {
+    currentPlayer = currentPlayer === Player2 ? Player1 : Player2;
+    turnText.textContent = `${currentPlayer.name}, make your turn!`;
+  }
 })();
-
-gameBoard.render();
